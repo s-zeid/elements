5e78e48ea5e3f22c3250b0fd41331cf7d745c8f1
Scott Zeid <s@zeid.me>
2019-02-28 02:35:40 -0600

    self-link h2-h6 headings with id's

--- static/main.js
+++ static/main.js
@@ -13,5 +13,14 @@
    if (!i.getAttribute("target"))
     i.setAttribute("target", "_blank");
   });
+  
+  document.querySelectorAll("h2[id], h3[id], h4[id], h5[id], h6[id]").forEach(function(i) {
+   var a = document.createElement("a");
+   a.setAttribute("href", "#" + i.getAttribute("id"));
+   i.childNodes.forEach(function(j) {
+    a.appendChild(j);
+   });
+   i.appendChild(a);
+  });
  });
 })();
