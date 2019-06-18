8270a91b5e31b1524f9cd3d9a6ab7d93049d19be
S. Zeid <s@zeid.me>
2019-06-18 03:50:46 -0500

    [main.js] fix incorrect childNodes iteration

--- static/main.js
+++ static/main.js
@@ -9,19 +9,23 @@
  onScroll();
  
  window.addEventListener("load", function() {
-  document.querySelectorAll("a[href^='https:'], a[href^='http:']").forEach(function(i) {
+  for (var i of document.querySelectorAll("a[href^='https:'], a[href^='http:']")) {
    if (!i.getAttribute("target"))
     i.setAttribute("target", "_blank");
-  });
+  }
   
-  document.querySelectorAll("h2[id], h3[id], h4[id], h5[id], h6[id]").forEach(function(i) {
+  for (var i of document.querySelectorAll("h2[id], h3[id], h4[id], h5[id], h6[id]")) {
    var a = document.createElement("a");
    a.setAttribute("href", "#" + i.getAttribute("id"));
-   i.childNodes.forEach(function(j) {
+   var children = [];
+   for (var j of i.childNodes) {
+    children.push(j);
+   }
+   for (var j of children) {
     a.appendChild(j);
-   });
+   }
    i.appendChild(a);
-  });
+  }
   
   if (document.querySelector("#contents + ul a"))
    new Gumshoe("#contents + ul a", {offset: 64, reflow: true});
